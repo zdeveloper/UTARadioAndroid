8d3cbee9293237d86a9eb831d918010df59ff41d
Zedd Shmais <mobprog@cse124erbmac3.local>
2013-10-04 19:28:50 -0500

    update
    updated where it shows

--- src/com/mobi/utaradioandroid/SplashActivity.java
+++ src/com/mobi/utaradioandroid/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.mobi.utaradioandroid;
 
 import android.os.Bundle;
+import android.view.Window;
 import android.app.Activity;
 import android.content.Intent;
 
@@ -10,19 +11,26 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         
         
-        int sleepTimeInSeconds = 3;
-        try {
-			Thread.sleep(sleepTimeInSeconds * 1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally{
-			Intent i = new Intent(SplashActivity.this, MainActivity.class);
-			startActivity(i);
-			finish();
-		}
+        new Thread(new Runnable() {
+            public void run() {
+            	int sleepTimeInSeconds = 3;
+                try {
+        			Thread.sleep(sleepTimeInSeconds * 1000);
+        		} catch (InterruptedException e) {
+        			// TODO Auto-generated catch block
+        			e.printStackTrace();
+        		} finally{
+        			Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        			startActivity(i);
+        			finish();
+        		}
+            }
+          }).start();
+        
+        
         
         
     }
